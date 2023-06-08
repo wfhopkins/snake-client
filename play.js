c4fdd46944058dc29a9f8785344ee19da8da408b
wfhopkins <will.hop@gmail.com>
2023-06-07 17:14:17 -0700

    created input file to manage hadnling user input

--- play.js
+++ play.js
@@ -1,21 +1,7 @@
 const {connect} = require("./client");
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = (key) => {
-  if (key === '\u0003') {
-    process.exit();
-  }
-}
+const {setupInput} = require("./input")
 
 console.log("Connecting ...");
+connect();
 
 setupInput();
-connect();
